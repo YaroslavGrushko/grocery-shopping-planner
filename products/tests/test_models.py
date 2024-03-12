79fd32d14f49dbd5b534d2a52cb84e4a6d986a0e
YaroslavGrushko <grushko.kpi@gmail.com>
2024-03-12 15:33:08 +0200

    Grocery-int: unit tests done (coverage 94%)

---
+++ products/tests/test_models.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from products.models import Category, Product
+from products.serializers import ProductSerializer
+from django.core.exceptions import ValidationError
+
+class ModelsTestCase(TestCase):
+    def test_product_negative_price(self):
+        user = User.objects.create(username="test")
+        category = Category.objects.create(user=user, name="test_category")
+        product = Product.objects.create(name="test_product", quantity=2, price=-1, category=category, )
+        
+        with self.assertRaises(ValidationError) as context:
+            product.full_clean()
+
+        self.assertEqual(context.exception.messages[0], 'Будь ласка вкажіть коректну ціну.')
+
+    def test_product_positive_price(self):
+        user = User.objects.create(username="test")
+        category = Category.objects.create(user=user, name="test_category")
+        product = Product.objects.create(name="test_product", quantity=2, price=20, category=category, )
+    
+        product.full_clean()
